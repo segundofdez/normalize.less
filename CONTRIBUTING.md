0ff9108f84a0603cb2e928fbbe0b49edbb08277e
Segundo Fdez <segun.2@gmail.com>
2015-05-29 23:21:22 +0200

    Update entire version from normalize.css 3.0.3

---
+++ CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# Contributing
+
+Contributions are welcome via [**Pull Requests**](https://github.com/segundofdez/normalize.less/pulls)
+
+## Bugs and issues
+
+If you see a problem or have an idea and is not addressed yet, please open a [new issue](https://github.com/segundofdez/normalize.less/issues)
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+/**
+* Gulp errors
+*/
+function swallowError (error) {
+    console.log(error.toString());
+    this.emit('end');
+}
+
+
+/**
+* Task custom normalize
+*/
+gulp.task('normalize', function () {
+    var less_src_import = 'normalize.less';
+    var less_dest_folder = 'dist/';
+
+    return gulp.src(less_src_import)
+        .pipe(less())
+        .on('error', swallowError)
+        .pipe(gulp.dest(less_dest_folder))
+});
